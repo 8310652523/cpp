f3e2832aad6798130e3e0af4f1a8878c5e2bd747
B Radha <radhabellary0305@gmail.com>
2025-08-26 08:36:13 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4, b=4;
+    int sum = a+b;
+    int diff = a-b;
+    float product = a*b;
+    cout << "the sum is: " << sum << endl;
+    cout << "the diff is: " << diff << endl;
+    cout << "the product is: " << product << endl;
+}
